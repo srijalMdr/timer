683cf1ffe78ffe0f8ae656f0952902b1ff151ba4
srijalMdr <srijalmdr7@gmail.com>
2025-01-11 15:00:49 +0545

    inital

--- script.js
+++ script.js
@@ -8,16 +8,24 @@ let enteredTime = null;
 const timerElement = document.getElementById("timer");
 const pauseResumeButton = document.querySelector(".control-buttons button");
 
-function startTimer() {
+const btnPause = document.querySelector(".pause");
+const btnRestart = document.querySelector(".restart");
+const btnChooseTime = document.querySelector(".chooseTime");
+
+const changeColor = function (bodyC, mainC) {
+  document.querySelector("body").style.backgroundColor = `${bodyC}`;
+  document.querySelector(".main").style.backgroundColor = `${mainC}`;
+};
+
+const startTimer = function () {
   //   timer = setInterval(updateTimer, 1000);
   if (!timer) {
     timer = setInterval(updateTimer, 1000);
-    document.querySelector("body").style.backgroundColor = "#86c232";
-    document.querySelector(".main").style.backgroundColor = "#A9C46C";
+    changeColor("#86c232", "#A9C46C");
   }
-}
+};
 
-function updateTimer() {
+const updateTimer = function () {
   timerElement.textContent = formatTime(minutes, seconds);
   if (minutes === 0 && seconds === 0) {
     alert("Time is up");
@@ -30,7 +38,7 @@ function updateTimer() {
       minutes--;
     }
   }
-}
+};
 
 function formatTime(minutes, seconds) {
   return `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(
@@ -45,9 +53,7 @@ function togglePauseResume() {
     clearInterval(timer);
     timer = null;
     pauseResumeButton.textContent = "Resume";
-    // document.querySelector("body").style.backgroundColor = "#D84040";
-    document.querySelector("body").style.backgroundColor = "#ED2B2A";
-    document.querySelector(".main").style.backgroundColor = "#F15A59";
+    changeColor("#ED2B2A", "#F15A59");
   } else {
     startTimer();
     pauseResumeButton.textContent = "Pause";
@@ -83,4 +89,18 @@ function chooseTime() {
   }
 }
 
+btnPause.addEventListener("click", function () {
+  console.log("clicked");
+  togglePauseResume();
+});
+
+btnChooseTime.addEventListener("click", function () {
+  console.log("c");
+  chooseTime();
+});
+
+btnRestart.addEventListener("click", function () {
+  console.log("r");
+  restartTimer();
+});
 startTimer();
